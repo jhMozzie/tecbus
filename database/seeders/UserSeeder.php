c1c45604e014041f45fa3e27dcf46213965af5c6
Joseph Flores <jhmozzie@gmail.com>
2023-12-09 18:45:04 -0500

    Actualizaciones
    Se actualizaron los seeders de usertype y user
    Se establecio que el tipo de usuario 4 es Conductor

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,58 +17,6 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'name' => 'Joseph',
-                'lastname' => 'Flores',
-                'email' => 'joseph@tecsup.edu.pe',
-                'dni' => 72299120,
-                'phone' => '123456789',
-                'password' => Hash::make('tengosed'),
-                'user_type_id' => 1, // Asegúrate de que el ID coincida con el tipo de usuario 'Estudiante'
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Kelly',
-                'lastname' => 'Castillo',
-                'email' => 'kelly@tecsup.edu.pe',
-                'dni' => 75599120,
-                'phone' => '946752980',
-                'password' => Hash::make('tengosed'),
-                'user_type_id' => 2, // Asegúrate de que el ID coincida con el tipo de usuario 'Profesor'
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Hirotoshi',
-                'lastname' => 'Santos',
-                'email' => 'hirotoshi@tecsup.edu.pe',
-                'phone' => '555555555',
-                'dni' => 70099120,
-                'password' => Hash::make('tengosed'),
-                'user_type_id' => 3, // Asegúrate de que el ID coincida con el tipo de usuario 'Administrador'
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Alvaro',
-                'lastname' => 'Lluvians',
-                'email' => 'lluvians@tecsup.edu.pe',
-                'phone' => '333333333',
-                'dni' => 70099100,
-                'password' => Hash::make('tengosed'),
-                'user_type_id' => 4, // Asegúrate de que el ID coincida con el tipo de usuario 'Administrador'
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
                 'name' => 'Profesor',
                 'lastname' => 'ProfesorLastname',
                 'email' => 'profesor@tecsup.edu.pe',
@@ -98,10 +46,23 @@ class UserSeeder extends Seeder
                 'name' => 'Admin',
                 'lastname' => 'AdminLastname',
                 'email' => 'admin@tecsup.edu.pe',
-                'dni' => 12345678,
+                'dni' => 12345670,
                 'phone' => '987654321',
                 'password' => Hash::make('tengosed'),
                 'user_type_id' => 3, // 3 es el ID para el tipo de usuario 'Administrador'
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Conductor',
+                'lastname' => 'ConductorLastname',
+                'email' => 'conductor@tecsup.edu.pe',
+                'dni' => 12345672,
+                'phone' => '987600401',
+                'password' => Hash::make('tengosed'),
+                'user_type_id' => 4, // 3 es el ID para el tipo de usuario 'Administrador'
                 'email_verified_at' => now(),
                 'remember_token' => Str::random(10),
                 'created_at' => now(),
